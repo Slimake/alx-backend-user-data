100d382eab6308c1d7ba929c9fef077c581125af
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-11-19 13:18:58 +0100

    Refactor user_id_for_session_id method

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -31,4 +31,4 @@ class SessionAuth(Auth):
         if type(session_id) is not str:
             return None
 
-        return self.user_id_by_session_id.get('session_id')
+        return self.user_id_by_session_id.get(session_id)
