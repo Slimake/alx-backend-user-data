7db539c4c12141bf91f76ee110e72dd0cbbbf337
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-11-21 14:05:33 +0100

    Handle all routes for Session authentication

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""session_auth Module
+"""
+from api.v1.views import app_views
+from flask import jsonify, request
+from os import getenv
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def auth_session() -> str:
+    """Handles all routes for the Session authentication
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if email is None or email == '':
+        return jsonify({"error": "email missing"}), 400
+    if password is None or password == '':
+        return jsonify({"error": "password missing"}), 400
+
+    from models.user import User
+    try:
+        user = User.search({'email': email})
+    except KeyError:
+        user = None
+    if not user:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    user = user[0]
+    if not user.is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+
+    from api.v1.app import auth
+    session_id = auth.create_session(user.id)
+    SESSION_NAME = getenv('SESSION_NAME')
+
+    response = jsonify(user.to_json())
+    response.set_cookie(SESSION_NAME, session_id)
+
+    return response
