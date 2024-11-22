c3423e20a393459bf62d04dc6b872e2b5951ec08
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-11-22 23:09:26 +0100

    Change user_id type from str to int

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -60,11 +60,11 @@ class DB:
 
         return result
 
-    def update_user(self, user_id: str, **kwargs: Dict) -> None:
+    def update_user(self, user_id: int, **kwargs: Dict) -> None:
         """Update the user’s attributes as passed in the method’s arguments
         """
         user = self.find_user_by(id=user_id)
-
+        print(type(user_id))
         for key, value in kwargs.items():
             if key not in user.__dict__:
                 raise ValueError
