a62dcd6dae8a2e9ebb875b0522cdfce08e895bc8
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-11-23 02:01:41 +0100

    Add type annotation to the function

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -16,7 +16,7 @@ def _hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password, bcrypt.gensalt())
 
 
-def _generate_uuid():
+def _generate_uuid() -> str:
     """Generate a uuid string
     """
     return str(uuid4())
