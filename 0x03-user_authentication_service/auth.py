3dee778152d7eb6b71637894c9acbafb5f6bd32e
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-11-24 14:24:23 +0100

    Add get_user_from_session_id method
    Return a user based on the session_id or None if the sesssion_id doesn't exist

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,6 +6,7 @@ from db import DB
 from sqlalchemy.orm.exc import NoResultFound
 from user import User
 from uuid import uuid4
+from typing import Union
 
 
 def _hash_password(password: str) -> bytes:
@@ -63,5 +64,18 @@ class Auth:
             session_id = _generate_uuid()
             user.session_id = session_id
             return session_id
+        except NoResultFound:
+            return None
+
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+        """Get the user of the session_id or
+        None if session_id doesn't belong to any user
+        """
+        if session_id is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
         except NoResultFound:
             return None
